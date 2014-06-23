2b759c62d8c4224ccc59cb57a928ad506be5ded1
vlad-ignatov <vlad.ignatov@gmail.com>
2014-06-23 10:10:20 +0300

    Separate parsers for table and field references

--- src/statements/select.js
+++ src/statements/select.js
@@ -1,4 +1,71 @@
 STATEMENTS.SELECT = function(walker) {
+
+	/**
+	 * Parses a table reference witch might be defined as "tableName" or 
+	 * as "databaseName.tableName". This function does NOT try to evaluate the
+	 * result to real Table object. Instead it just returns an object with 
+	 * "table" and "database" properties (the "database" will be null if not 
+	 * defined). 
+	 * @return {Object}
+	 * @throws {SQLParseError} if the input cannot be parsed correctly
+	 */
+	function tableRef() 
+	{
+		var out = {
+			database : null, 
+			table    : null
+		};
+
+		walker.someType(WORD_OR_STRING, function(token) {
+			out.table = token[0];
+		})
+		.optional(".", function() {
+			walker.someType(WORD_OR_STRING, function(token) {
+				out.database = out.table;
+				out.table    = token[0];
+			});
+		});
+
+		return out;
+	}
+
+	/**
+	 * Parses a table field reference witch might be defined as "fieldName" or 
+	 * as "tableName.fieldName", or as "databaseName.tableName.fieldName". This 
+	 * function does NOT try to evaluate the result to real field object. 
+	 * Instead it just returns an object with "field", "table" and "database" 
+	 * properties (the "database" and "table" will be null if not defined). 
+	 * @return {Object}
+	 * @throws {SQLParseError} if the input cannot be parsed correctly
+	 */
+	function fieldRef() 
+	{
+		var out = {
+			database : null, 
+			table    : null,
+			field    : null
+		};
+
+		walker.someType(WORD_OR_STRING, function(token) {
+			out.field = token[0];
+		})
+		.optional(".", function() {
+			walker.someType(WORD_OR_STRING, function(token) {
+				out.table = out.field;
+				out.field = token[0];
+			})
+			.optional(".", function() {
+				walker.someType(WORD_OR_STRING, function(token) {
+					out.database = out.table;
+					out.table    = out.field;
+					out.field    = token[0];
+				});
+			});
+		});
+
+		return out;
+	}
+
 	return function() {
 		
 		var tableName, dbName, table;
@@ -8,20 +75,11 @@ STATEMENTS.SELECT = function(walker) {
 				
 				walker.pick({
 					"FROM" : noop
-				})
-				
-				// table -------------------------------------------------------
-				.someType(WORD_OR_STRING, function(token) {
-					tableName = token[0];
-				})
-				.optional(".", function() {
-					walker.someType(WORD_OR_STRING, function(token) {
-						dbName = tableName;
-						tableName = token[0];
-					});
 				});
 				
-				table = getTable(tableName, dbName);
+				// table -------------------------------------------------------
+				var tbl   = tableRef(),
+					table = getTable(tbl.table, tbl.database);
 				
 				walker.errorUntil(";")
 				.commit(function() {
