bfea66b894cfcf6c7a940ceddb4823704537e709
vlad-ignatov <vlad.ignatov@gmail.com>
2014-06-26 15:52:56 +0300

    Performance improvements for CROSS JOIN

--- jsdb-2.js
+++ jsdb-2.js
@@ -664,8 +664,98 @@ function defaultErrorHandler(e)
 }
 
 // JOIN functions --------------------------------------------------------------
+function LinkedListNode(data)
+{
+	this.data = data;
+}
+
+LinkedListNode.prototype = {
+	prev : null,
+	next : null,
+	before : function(data) {
+		var node = new LinkedListNode(data);
+		if (this.prev) {
+			node.prev = this.prev;
+			this.prev.next = node;
+		}
+		this.prev = node;
+		node.next = this;
+		return node;
+	},
+	after : function(data) {
+		var node = new LinkedListNode(data);
+		if (this.next) {
+			node.next = this.next;
+			this.next.prev = node;
+		}
+		this.next = node;
+		node.prev = this;
+		return node;
+	}
+};
+
+function crossJoinUsingLinkedList(tables) 
+{
+	var tl = tables.length,
+		rows = [],
+		left, 
+		right, 
+		row, 
+		row0,
+		rowId,
+		prev,
+		next,
+		first,
+		cur,
+		i = 0,
+		l = 0,
+		y;
+
+	while ( i < tl )
+	{
+		right = tables[i++];
+
+		if (i === 1) {
+			for ( rowId in right.rows )
+			{
+				if (!first) {
+					first = new LinkedListNode(right.rows[rowId]._data.slice());
+					prev  = first;
+				} else {
+					prev  = prev.after(right.rows[rowId]._data.slice());
+				}
+			}
+		} else {
+			row = first;
+			while ( row ) {
+				y = 0;
+				row0 = row.data;
+				for ( rowId in right.rows )
+				{
+					if (++y === 1) {
+						row.data = row0.concat( right.rows[rowId]._data );
+					} else {
+						row = row.after(row0.concat( right.rows[rowId]._data ));
+					}
+				}
+				row = row.next;
+			}
+		}
+	}
+
+	row = first;
+	while ( row ) {
+		rows.push(row.data);
+		row = row.next;
+	}
+	//console.dir(first);
+	//console.dir(rows);
+	return rows;
+}
+
 function crossJoin(tables) 
 {
+	//crossJoinUsingLinkedList(tables);
 	console.time("crossJoin");
 	var _tables = tables.slice(),
 		tl = _tables.length,
@@ -679,7 +769,7 @@ function crossJoin(tables)
 			left = [];
 			for ( rowId in right.rows )
 			{
-				l = left.push(right.rows[rowId]._data);
+				l = left.push(right.rows[rowId]._data.slice());
 			}
 			continue;
 		}
@@ -687,7 +777,7 @@ function crossJoin(tables)
 		for ( i = 0; i < l; i++ ) 
 		{
 			y = 0;
-			row0 = left[i].slice();
+			row0 = left[i];
 			for ( rowId in right.rows )
 			{
 				row = row0.concat(right.rows[rowId]._data);
