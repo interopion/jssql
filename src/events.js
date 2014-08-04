ae239579531311dfd356216c8ea203492ae40716
vlad-ignatov <vlad.ignatov@gmail.com>
2014-08-04 14:49:26 +0300

    Some improvements for the event system

--- src/events.js
+++ src/events.js
@@ -1,28 +1,77 @@
+/**
+ * The event system
+ * @namespace events
+ * @type {Object}
+ */
 var events = (function() {
 	
 	var listeners = {};
 
+	function returnFalse()
+	{
+		return false;
+	}
+
+	/**
+	 * Adds an event listener
+	 * @param {String} eType The event type to listen for
+	 * @param {Function|Boolean} handler The function to be invoked. Can also be 
+	 * a falsy value which will be internally converted to a function that 
+	 * returns false.
+	 * @return {Function} The bound event handler
+	 */
 	function bind(eType, handler) 
 	{
+		if (handler === false)
+			handler = returnFalse;
+
 		if (!listeners[eType])
 			listeners[eType] = [];
+		
 		listeners[eType].push(handler);
 		return handler;
 	}
 
+	/**
+	 * Adds an event listener that removes itself after the first call to it
+	 * @param {String} eType The event type to listen for
+	 * @param {Function|Boolean} handler The function to be invoked. Can also be 
+	 * a falsy value which will be internally converted to a function that 
+	 * returns false.
+	 * @return {Function} The bound event handler
+	 */
 	function one(eType, handler) 
 	{
+		if (handler === false)
+			handler = returnFalse;
+
 		function fn(data) {
 			var out = handler(data);
 			unbind(eType, fn);
 			return out;
-		} 
+		}
+
 		bind(eType, fn);
 		return fn;
 	}
 
+	/**
+	 * Removes event listener(s)
+	 * 1. If the method is called with no arguments removes everything.
+	 * 2. If the method is called with one argument removes everything for that
+	 *    event type.
+	 * 3. If the method is called with two arguments removes the specified 
+	 *    handler from the specified event type.
+	 * @param {String} eType The event type
+	 * @param {Function|Boolean} handler The function to be removed. Can also be 
+	 * a false to remove the generic "returnFalse" listeners.
+	 * @return {void}
+	 */
 	function unbind(eType, handler) 
 	{
+		if (handler === false)
+			handler = returnFalse;
+
 		if (!eType) {
 			listeners = {};
 		} else if (!handler) {
@@ -60,7 +109,9 @@ var events = (function() {
 		dispatch : dispatch,
 		bind     : bind,
 		unbind   : unbind,
-		one      : one
+		one      : one,
+		on       : bind,
+		off      : unbind
 	};
 
 })();
