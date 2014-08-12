c77ab9db5fa2fca61ba47a78034a34309f008399
vlad-ignatov <vlad.ignatov@gmail.com>
2014-08-12 12:14:11 +0300

    The "CREATE DATABASE" statement parser now returns a Task

--- src/statements/create_database.js
+++ src/statements/create_database.js
@@ -6,19 +6,43 @@
  * @return {void}
  */
 STATEMENTS.CREATE_DATABASE = function(walker) {
-	return function() {
-		var q = new CreateDatabaseQuery();
-		walker
-		.optional("IF NOT EXISTS", function() {
-			q.ifNotExists(true);
-		})
-		.someType(WORD_OR_STRING, function(token) {
-			q.name(token[0]);
-		})
-		.nextUntil(";")
-		.commit(function() {
-			q.execute();
-			walker.onComplete('Database "' + q.name() + '" created.');
-		});
-	};
+
+	// remember the databse name here so that we can undo
+	var dbName;
+
+	function undo(done, fail) {
+		if (dbName) {
+			SERVER.dropDatabase(dbName, true, done, fail);
+		} else {
+			done();
+		}
+	}
+
+	return new Task({
+		name : "Create Database",
+		execute : function(done, fail) {
+			var q = new CreateDatabaseQuery();
+
+			// Make sure to reset this in case it stores something from 
+			// previous query
+			dbName = null;
+
+			walker
+			.optional("IF NOT EXISTS", function() {
+				q.ifNotExists(true);
+			})
+			.someType(WORD_OR_STRING, function(token) {
+				q.name(token[0]);
+			})
+			.nextUntil(";")
+			.commit(function() {
+				dbName = q.name();
+				q.execute();
+				done('Database "' + q.name() + '" created.');
+			});
+		},
+		undo : function(done, fail) {
+			undo(done, fail);
+		}
+	});
 };
