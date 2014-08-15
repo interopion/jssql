e94862c1f5cc28a4fee165751274f6dec2be8075
vlad-ignatov <vlad.ignatov@gmail.com>
2014-08-15 12:32:13 +0300

    Conflict resolution for UPDATE queries

--- src/Table.js
+++ src/Table.js
@@ -74,6 +74,7 @@ function Table(tableName, db)
 Table.prototype = new Persistable();
 Table.prototype.constructor = Table;
 
+/*
 Table.prototype.createIndex = function(options) 
 {
 	var name;
@@ -89,7 +90,14 @@ Table.prototype.createIndex = function(options)
 		onConflict : null
 	};
 };
+*/
 
+/**
+ * Generates and returns the JSON representation of the table object. This is 
+ * mostly used by the "save procedure".
+ * @todo The rows property might contain only the IDs instead of full keys
+ * @return {Object}
+ */
 Table.prototype.toJSON = function() 
 {
 	var json = {
@@ -122,14 +130,24 @@ Table.prototype.getStorageKey = function()
 	return [NS, this._db.name, this.name].join(".");
 };
 
+/**
+ * Add constraint to the table. This can be used if the columns are already
+ * known and created. Creates an index and updates the Column object(s) to refer
+ * to it...
+ */
 Table.prototype.addConstraint = function(props)
 {
-	if (props.type == TableIndex.TYPE_INDEX ||
+	if (props.type == TableIndex.TYPE_INDEX  ||
 		props.type == TableIndex.TYPE_UNIQUE ||
 		props.type == TableIndex.TYPE_PRIMARY) 
 	{
 		var key = TableIndex.fromJSON(props, this);
 		this.keys[key.name] = key;
+		
+		for (var i = 0, l = props.columns.length; i < l; i++)
+		{
+			this.cols[props.columns[i]].setKey(props.type);
+		}
 	}
 };
 
@@ -307,6 +325,207 @@ Table.prototype.insert = function(keys, values)
 
 	//console.dir(this.toJSON());
 };
+
+/**
+ * Updates table row(s)
+ */
+Table.prototype.update = function(map, alt, where, onSuccess, onError)
+{
+	// The UPDATE can be canceled if a "before_update:table" listener returns false 
+	if (!JSDB.events.dispatch("before_update:table", this)) {
+		onError(new SQLRuntimeError(
+			'The UPDATE procedure of table "%s" was canceled by a "before_update:table" event listener',
+			this.getStorageKey()
+		));
+		return;
+	}
+	
+	var table = this, 
+		trx = new Transaction({
+			name         : "Update " + table.name + " transaction",
+			autoRollback : false,
+			onError      : handleConflict,
+			onComplete   : onSuccess
+		});
+
+	// ROLLBACK|ABORT|REPLACE|FAIL|IGNORE
+	function handleConflict(error)
+	{
+		if (error && error instanceof SQLConstraintError) 
+		{
+			switch (alt) {
+
+				// When an applicable constraint violation occurs, the ROLLBACK 
+				// resolution algorithm aborts the current SQL statement with an 
+				// SQLConstraintError and rolls back the current transaction. 
+				// If no transaction is active (other than the implied 
+				// transaction that is created on every command) then the 
+				// ROLLBACK resolution algorithm works the same as the ABORT 
+				// algorithm.
+				case "ROLLBACK":
+					trx.setOption("reqursiveRollback", true);
+					trx.one("rollback", function() {
+						if (onError) onError(error);
+						console.info("Update lolled back!");
+					});
+					trx.rollback();
+				break;
+
+				// When an applicable constraint violation occurs, the FAIL 
+				// resolution algorithm aborts the current SQL statement with an 
+				// SQLConstraintError. But the FAIL resolution does not back out 
+				// prior changes of the SQL statement that failed nor does it 
+				// end the transaction. For example, if an UPDATE statement 
+				// encountered a constraint violation on the 100th row that it 
+				// attempts to update, then the first 99 row changes are 
+				// preserved but changes to rows 100 and beyond never occur.
+				case "FAIL":
+					if (onError) 
+						onError(error);
+				break;
+
+				// When an applicable constraint violation occurs, the IGNORE 
+				// resolution algorithm skips the one row that contains the 
+				// constraint violation and continues processing subsequent rows 
+				// of the SQL statement as if nothing went wrong. Other rows 
+				// before and after the row that contained the constraint 
+				// violation are inserted or updated normally. No error is 
+				// returned when the IGNORE conflict resolution algorithm is used.
+				case "IGNORE":
+					trx.next();
+				break;
+
+				// 
+				case "REPLACE":
+				break;
+
+				// When an applicable constraint violation occurs, the ABORT 
+				// resolution algorithm aborts the current SQL statement with an 
+				// SQLConstraintError error and backs out any changes made by 
+				// the current SQL statement; but changes caused by prior SQL 
+				// statements within the same transaction are preserved and the 
+				// transaction remains active. This is the default behavior and 
+				// the behavior specified by the SQL standard.
+				default: // ABORT
+					trx.setOption("reqursiveRollback", false);
+					trx.one("rollback", function() {
+						if (onError) onError(error);
+						console.info("Update lolled back!");
+					});
+					trx.rollback();
+				break;				
+			}
+		}
+		else
+		{
+			if (onError) 
+				onError(error);
+		}
+	}
+
+	each(table.rows, function(row, id) {
+		//debugger;
+		var newRow = row.toJSON(), name;
+
+		// Skip rows that don't match the WHERE condition if any
+		if (where && !executeCondition( where, newRow ))
+		{
+			return true;
+		}
+
+		// Create the updated version of the row
+		//for ( name in map )
+		//{
+		//	newRow[name] = executeCondition(map[name], newRow);
+		//}
+
+		// The UPDATE can be canceled on row level if a "before_update:row" 
+		// listener returns false 
+		//if (!JSDB.events.dispatch("before_update:row", row))
+		//{
+		//	return true;
+		//}
+
+		// Update table indexes
+		//for (var ki in table.keys) 
+		//{
+		//	table.keys[ki].beforeUpdate(row, newRow);
+		//}
+		
+		// Update the actual row
+		//for ( name in map )
+		//{
+		//	row.setCellValue( name, newRow[name] );
+		//}
+
+		(function(row, newRow) {
+
+			var rowBackUp = row.toJSON();
+			
+			var task = Transaction.createTask({
+				name : "Update row " + row.getStorageKey(),
+				execute : function(done, fail)
+				{
+					var name;
+
+					// Create the updated version of the row
+					for ( name in map )
+					{
+						newRow[name] = executeCondition(map[name], newRow);
+					}
+
+					// The UPDATE can be canceled on row level if a 
+					// "before_update:row" listener returns false 
+					if (!JSDB.events.dispatch("before_update:row", row))
+					{
+						done();
+						return true;
+					}
+
+					// Update table indexes
+					for (var ki in table.keys) 
+					{
+						table.keys[ki].beforeUpdate(row, newRow);
+					}
+
+					// Update the actual row
+					for ( name in map )
+					{
+						row.setCellValue( name, newRow[name] );
+					}
+
+					done();
+				},
+				undo : function(done)
+				{
+					for ( var name in rowBackUp )
+						row.setCellValue( name, rowBackUp[name] );
+					done();
+				}
+			});
+
+			trx.add(task);
+
+		})(row, newRow);
+	});
+	
+	trx.start();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Table.prototype.drop = function(onComplete, onError) 
 {
