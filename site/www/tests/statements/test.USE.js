42df3d9a45c0277802f4b08b43870a72d1cad947
vlad-ignatov <vlad.ignatov@gmail.com>
2014-09-01 20:01:56 +0300

    Fixed the tests for the USE statement to use the new public api

--- site/www/tests/statements/test.USE.js
+++ site/www/tests/statements/test.USE.js
@@ -3,70 +3,60 @@
 	module("USE", {
 		setup: function() {
 			QUnit.stop();
-			if (!JSDB.SERVER.loaded) {
-				JSDB.events.one("load:server", function() {
-					JSDB.query("CREATE DATABASE IF NOT EXISTS unitTestingDB;", function() {
-						QUnit.start();
-					});
-				});
-			} else {
-				JSDB.query("CREATE DATABASE IF NOT EXISTS unitTestingDB;", function() {
+			jsSQL(function(api) {
+				api.query("CREATE DATABASE IF NOT EXISTS unitTestingDB", function(err) {
+					if (err)
+						QUnit.pushFailure("setup failed: " + err);
 					QUnit.start();
 				});
-			}
+			});
 		},
 		teardown: function() {
 			QUnit.stop();
-			JSDB.query("DROP DATABASE IF EXISTS unitTestingDB;", function() {
-				QUnit.start();
+			jsSQL(function(api) {
+				api.query("DROP DATABASE IF EXISTS unitTestingDB", function(err) {
+					if (err)
+						QUnit.pushFailure("setup failed: " + err);
+					QUnit.start();
+				});
 			});
 		}
 	});
 
 	QUnit.asyncTest("USE unitTestingDB", function(assert) {
-		var queries = [
-				"USE unitTestingDB;",
-				"USE \"unitTestingDB\";",
-				"USE 'unitTestingDB';",
-				"USE `unitTestingDB`;"
+		var sql = [
+				"USE unitTestingDB",
+				"USE \"unitTestingDB\"",
+				"USE 'unitTestingDB'",
+				"USE `unitTestingDB`"
 			],
-			len = queries.length,
-			sql = queries.join("");
+			len = sql.length;
 
-		JSDB.query(
-			sql, 
-			function(result, queryIndex) {
-				var db = JSDB.SERVER.getCurrentDatabase();
+		jsSQL(function(api) {
+			api.query(sql, function(err, result, queryIndex) {
+				var db = api.getCurrentDatabase();
+
+				if (err)
+					QUnit.pushFailure(err, sql[queryIndex]);
+
 				assert.ok(
 					db && db.name == "unitTestingDB", 
-					'Query ' + queries[queryIndex] + ' selects a database named "unitTestingDB"'
+					'Query ' + sql[queryIndex] + ' selects a database named "unitTestingDB"'
 				);
+
 				if (queryIndex == len - 1)
 					QUnit.start();
-			}, 
-			function(error, queryIndex) {
-				QUnit.pushFailure(error.message || "Failed", sql);
-				if (queryIndex == len - 1)
-					QUnit.start();
-			}
-		);
+			});
+		});
 	});
 
 	QUnit.asyncTest("USE NonExistingDB", function(assert) {
-		var sql = "USE NonExistingDB;";
-		//expect(0);
-		JSDB.query(
-			sql, 
-			function() {
-				QUnit.pushFailure("Failed (called onSuccess callback while the DB does not exist)", sql);
-				if (QUnit.config.semaphore)
-					QUnit.start();
-			}, 
-			function(error) {
-				QUnit.push(true, error, error, "Must fail: " + error);
-				if (QUnit.config.semaphore)
-					QUnit.start();
-			}
-		);
+		var sql = "USE NonExistingDB";
+		jsSQL(function(api) {
+			api.query(sql, function(err, result) {
+				QUnit.ok(!!err, "Failed as expected (" + sql + ")");
+				QUnit.start();
+			});
+		});
 	});
 })();
